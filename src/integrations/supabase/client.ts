c0ba50e35532267de5fa54112cd2b382192c74cb
GrandecapoDave <davide.pregliasco@gmail.com>
2026-08-18 10:33:27 +0200

    chore: align client credentials to point to new database project

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL = import.meta.env["VITE_SUPABASE_URL"] || "https://aehjyuarqvwovqfodlss.supabase.co";
-const SUPABASE_KEY = import.meta.env["VITE_SUPABASE_PUBLISHABLE_KEY"] || "sb_publishable_JPNkHg4UFgBYnasfD2c23g_4vtHSSCm";
+const SUPABASE_URL = import.meta.env["VITE_SUPABASE_URL"] || "https://mbomqxuwmbtxcogbuugr.supabase.co";
+const SUPABASE_KEY = import.meta.env["VITE_SUPABASE_PUBLISHABLE_KEY"] || "sb_publishable_RY-bi0J_MaPirLG0oDSJiQ_7TPEj7Bq";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
